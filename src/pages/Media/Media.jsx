6d565e99bbbd4e506ce786d57016848190082ef0
Hubert2047 <84777125+Hubert2047@users.noreply.github.com>
2022-05-04 01:11:58 +0800

    push media footer

--- src/pages/Media/Media.jsx
+++ src/pages/Media/Media.jsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { React, useState } from 'react'
+import { React, useRef, useState } from 'react'
 import { BsSkipEnd, BsSkipStart, BsVolumeUpFill } from 'react-icons/bs'
 import { IoIosPause, IoMdVolumeOff } from 'react-icons/io'
 import { IoCaretBackCircleOutline } from 'react-icons/io5'
@@ -9,24 +9,77 @@ import styles from './Media.module.scss'
 export default function Media() {
     const [isPause, setIsPause] = useState(true)
     const handleInputBar = (e) => {
-        console.log(e.target.value)
+        // audioRef.current.currentTime=
     }
+    const [audioSetting, setAudioSetting] = useState({
+        currentTime: '00:00',
+        endTime: '00:00',
+        currentProces: 0,
+    })
+    const audioRef = useRef()
     const handleVolumeBar = (e) => {
         console.log(e.target.value)
     }
+    const thumpStyle = { width: `${audioSetting.currentProces}%` }
+    const dotStyle = { left: `${audioSetting.currentProces - 2}%` }
+    const handleTimeUpdate = () => {
+        let audioCurrentTime = audioRef.current.currentTime
+        let audioDuration = audioRef.current.duration
+        let currentTimeMin = Math.floor(audioCurrentTime / 60)
+        currentTimeMin =
+            currentTimeMin < 10 ? `0${currentTimeMin}` : currentTimeMin
+        let currentTimeSec = Math.floor(audioCurrentTime % 60)
+        currentTimeSec =
+            currentTimeSec < 10 ? `0${currentTimeSec}` : currentTimeSec
+        let endTimeMin = Math.floor((audioDuration - audioCurrentTime) / 60)
+        endTimeMin = currentTimeMin < 10 ? `0${endTimeMin}` : endTimeMin
+        let endTimeSec = Math.floor((audioDuration - audioCurrentTime) % 60)
+        endTimeSec = currentTimeSec < 10 ? `0${endTimeSec}` : endTimeSec
+        let currentProces = Math.floor((audioCurrentTime / audioDuration) * 100)
+        setAudioSetting({
+            currentTime: `${currentTimeMin}:${currentTimeSec}`,
+            endTime: `${endTimeMin}:${endTimeSec}`,
+            currentProces: currentProces,
+        })
+    }
     const handleOnOff = () => {
+        if (isPause) {
+            audioRef.current.play()
+        } else {
+            audioRef.current.pause()
+        }
         setIsPause((prev) => !prev)
     }
     return (
         <div className={styles.media}>
+            <audio
+                ref={audioRef}
+                onTimeUpdate={handleTimeUpdate}
+                src='./nhactest.mp3'></audio>
             <div className={styles.main}></div>
             <div className={clsx(styles.footer, 'd-flex-r')}>
                 <div className={clsx(styles.left, 'd-flex-r')}>
                     <div className={clsx(styles.waves, 'd-flex-r')}>
-                        <div className={clsx(styles.wave, styles.wave1)}></div>
-                        <div className={clsx(styles.wave, styles.wave2)}></div>
-                        <div className={clsx(styles.wave, styles.wave3)}></div>
-                        <div className={clsx(styles.wave, styles.wave4)}></div>
+                        <div
+                            className={clsx(styles.wave, styles.wave1, {
+                                [styles.waveAnimated]: !isPause,
+                            })}></div>
+                        <div
+                            className={clsx(styles.wave, styles.wave2, {
+                                [styles.waveAnimated]: !isPause,
+                            })}></div>
+                        <div
+                            className={clsx(styles.wave, styles.wave3, {
+                                [styles.waveAnimated]: !isPause,
+                            })}></div>
+                        <div
+                            className={clsx(styles.wave, styles.wave4, {
+                                [styles.waveAnimated]: !isPause,
+                            })}></div>
+                        <div
+                            className={clsx(styles.wave, styles.wave5, {
+                                [styles.waveAnimated]: !isPause,
+                            })}></div>
                     </div>
                     <div className={styles.songCard}>
                         <MusicCard />
@@ -61,7 +114,9 @@ export default function Media() {
                         />
                     </div>
                     <div className={clsx(styles.thumpBox, 'd-flex-r')}>
-                        <span className={styles.startTime}>00:00</span>
+                        <span className={styles.startTime}>
+                            {audioSetting.currentTime}
+                        </span>
                         <div className={styles.bar}>
                             <input
                                 onChange={handleInputBar}
@@ -71,10 +126,14 @@ export default function Media() {
                                 max={100}
                                 className={styles.inputThump}
                             />
-                            <div className={styles.thump}></div>
-                            <div className={styles.dot}></div>
+                            <div
+                                style={thumpStyle}
+                                className={styles.thump}></div>
+                            <div style={dotStyle} className={styles.dot}></div>
                         </div>
-                        <span className={styles.endTime}>00:00</span>
+                        <span className={styles.endTime}>
+                            {audioSetting.endTime}
+                        </span>
                     </div>
                 </div>
                 <div className={styles.right}>
